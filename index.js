c4ff42204cce409d9cebd8f8c858dccb6aa8d7b6
Steve <stevenjameswatkin@gmail.com>
2018-04-30 17:42:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function sayHiToGrandma(string) {
   if(string.toLowerCase() === (string))
   return "I can't hear you!"
   if(string === "I love you, Grandma.")
-  return "I love you too."
+  return "I love you, too."
 }
