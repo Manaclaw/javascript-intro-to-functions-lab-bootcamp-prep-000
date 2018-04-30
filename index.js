8d367ea5a0f737bac991511368b24bb0e8273d66
Steve <stevenjameswatkin@gmail.com>
2018-04-30 17:26:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-function shout(string) {
-  return string.toUpperCase()
-}
-
-function whisper(string) {
-  return string.toLowerCase()
-}
-
-function logShout(string) {
-  console.log(string.toUpperCase)
-}
-
-function logWhisper(string) {
-  console.log(string.toLowerCase)
-}
-var love = "I love you, Grandma."
-
-var uppercase = "HELLO!"
-uppercase.toUpperCase() === uppercase // true
-var lowercase = 'hello!'
-lowercase.toLowerCase() === lowercase // true
-var mixedCase = "Hi there!"
-mixedCase.toLowerCase() === mixedCase // false
-mixedCase.toUpperCase() === mixedCase // false
-
-function sayHiToGrandma(string) {
-  if(string) = lowercase
-  return "I can't hear you!";
-  if(string) = uppercase
-  return "YES INDEED!";
-  if(string) = love
-  return "I love you, too."
 
 
   
